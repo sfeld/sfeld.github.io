c50bb4e7843829f029fbafe6f1fdeb391342ccb6
Sam Feldman <samfeldman@Sams-MacBook-Pro-2.local>
2019-12-29 11:37:38 -1000

    functional page

--- TheList.js
+++ TheList.js
@@ -2,5 +2,7 @@
 // var dt = require( 'datatables.net' )();
 
 $(document).ready( function () {
-    $('#table_id').DataTable();
+    $('#table_id').DataTable({
+        "paging":         false,
+    });
 } );
